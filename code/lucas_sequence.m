c9dd60825b7e09da0887c35c7a242a4df3d16d98
Sam Adams <samuel.adams@utah.edu>
2024-01-30 14:22:40 -0700

    Add documentation to lucas_sequence

--- code/lucas_sequence.m
+++ code/lucas_sequence.m
@@ -1,6 +1,60 @@
 function l = lucas_sequence(n, type, P, Q)
-% Reference https://en.wikipedia.org/wiki/Lucas_sequence for more
-% informatin about Lucas sequences.
+% Compute values in a Lucas sequence.
+%
+%  l = lucas_sequence(n)
+%  l = lucas_sequence(n, type, P, Q)
+%
+% Parameters
+% ----------
+% n : List of indices
+%   Query indices; which elements in the Lucas sequence should be returned.
+%   Can include zero.
+% type : {'U', 'V'}, optional
+%   The type of sequence to generate. Default is 'U'.
+% P : scalar, optional
+%   Value for coefficient $P$. Default is 1.
+% Q : scalar, optional
+%   Value for coefficient $Q$. Default is -1.
+%
+% Returns
+% -------
+% l : List of numerics
+%   Lucas numbers for index `n`.
+%
+% Notes
+% -----
+% Lucas sequences define two types of sequences that have the recurrence
+% relation:
+%
+% $$ x_n = P \cdot x_{n-1} - Q \cdot x_{n-2}$$
+%
+% The first two entries in the $U$-type sequence are $U_0(P,Q)=0$ and
+% $U_1(P,Q)=1$. The first two entries for a $V$-type sequence are
+% $V_0(P,Q)=2$ and $V_1(P,Q)=P$.
+%
+% The Fibonacci sequence is a special case of the $U$-type Lucas sequence, 
+% with $P=1, Q=-1$.
+%
+% References
+% ----------
+% [1]. https://en.wikipedia.org/wiki/Lucas_sequence
+%
+% Examples
+% --------
+% >> lucas_sequence(0:5)
+% ans = 
+%   0   1   1   2   3   5
+% >> lucas_sequence([1, 2, 10], 'V', 1, 2)
+% ans =
+%   1   -3  57
+%
+% See also
+% --------
+% fibonacci
+%
+% Contributors
+% ------------
+% Samuel Adams-Tew
 arguments
     n(1,:) {mustBeInteger, mustBeNonnegative}
     type string = 'U'
